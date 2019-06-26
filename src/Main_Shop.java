ec241f8bab72bfdc3dd334a29b26a438fa7b5c9f
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-06-26 21:05:57 +0100

    Commit. first

---
+++ src/Main_Shop.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main_Shop extends Application {
+
+	public Main_Shop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
