43448d7c26373b4ee37050b26e59049b6b2908e6
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-06-27 15:42:06 +0100

    Commit 11 - not ignore

--- src/Main_Shop.java
+++ src/Main_Shop.java
@@ -1,21 +1,49 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main_Shop extends Application {
 
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+
+//import layout 
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
+//Import visual components
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
+
+/** Creating the visual of the shop */
+public class Main_Shop extends Application {
+	
+	/** The visual constructor*/
 	public Main_Shop() {
-		// TODO Auto-generated constructor stub
-	}
+
+	}//closing constructor oh the chocolate
+	
+	/** Create the override init to initiate the visual variables*/
+	@Override
+	public void init() {
+		
+	}//closing init()
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage chocolate) throws Exception {
 
-	}
 
+	}//closing start of the Stage (Chocolate
+
+	//open the main method of the chocolate
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
 
-}
+	}//closing the main method
+
+}//closing the class of the application MAIN_Shop
